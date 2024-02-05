508a613562ece34446daca22060ac89a5c4d83f3
Davi <ed.miranda@tutanota.com>
2024-02-05 15:21:44 -0800

    first commit

--- src/app/escala_de_braden/index.jsx
+++ src/app/escala_de_braden/index.jsx
@@ -1,11 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
+import {Link} from 'expo-router'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+        <Text>tela escala de braden</Text>
+        <Link>
+          Toucha
+        </Link>
     </View>
   );
 }
---
+++ src/app/index.jsx
@@ -0,0 +1,34 @@
+import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
+import {Link} from 'expo-router'
+import Button from '../components/Button';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+        <View style={styles.header}>
+            <Image source={require('../images/lampada_logo.jpg')} style={styles.imagemLogo}/>
+            <Text>TESTE</Text>
+        </View>
+
+        <Text>OLA MUNDO</Text>
+
+        <Button texto='Escala de teste' link='escala_de_braden/'/>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  header: {
+    backgroundColor: '#1A998E',
+    padding: 20,
+  },
+  imagemLogo: {
+    width: 80,
+    height: 80,
+  },
+});
